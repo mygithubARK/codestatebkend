2ecc625668ce098ff708867a4b202ba90f90a8b2
Ramaiah Kothandaramaiah <ramaiahkothandar@media-saturn.com>
2019-10-22 14:02:56 +0200

    Initial commit

---
+++ src/test/java/com/savindi/codestatebkend/CodestatebkendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savindi.codestatebkend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodestatebkendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
